184bb2c3c6403f0b7d40a82611ad51dc1a95f450
thiagocalvi <thiagohcalvi@gmail.com>
2022-08-11 00:14:47 -0300

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 8080;
+const port = 80;
 const session = require('express-session');
 
 
